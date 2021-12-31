a6d581d88cc4e7a18a3843ac3677f548300c78d2
김범진 <sdpthf@naver.com>
2021-12-31 14:37:20 +0900

    ⚡ :: UserService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,23 +1,40 @@
 import { Injectable } from '@nestjs/common';
-
-export type User = any;
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { HttpStatus } from '@nestjs/common';
+import { ForbiddenException } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
-    private readonly users = [
-        {
-          userId: 1,
-          username: 'john',
-          password: 'changeme',
-        },
-        {
-          userId: 2,
-          username: 'maria',
-          password: 'guess',
-        },
-      ];
-    
-    async findOne(username: string): Promise<User | undefined> {
-        return this.users.find(user => user.username === username);
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+    ) { }
+
+    async create(createUserDto): Promise<any> {
+        const isExist = await this.userRepository.findOne({ userId: createUserDto.userId });
+        if (isExist) {
+            throw new ForbiddenException({
+                statusCode: HttpStatus.FORBIDDEN,
+                message: [`이미 등록된 사용자입니다.`],
+                error: 'Forbidden'
+            })
+        }
+
+        const { password, ...result } = await this.userRepository.save(createUserDto);
+        return result;
+    }
+
+    async findAll(): Promise<User[]> {
+        return this.userRepository.find({
+            select: ["seq", "userId", "userName", "role"],
+        });
+    }
+
+    findOne(id: string): Promise<User> {
+        return this.userRepository.findOne(id, {
+            select: ["seq", "userId", "userName", "role"],
+        });
     }
 }
