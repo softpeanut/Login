7d6d2476e048e7b1a6ee46b85456ebacc9bbdd9e
김범진 <sdpthf@naver.com>
2021-12-31 14:37:08 +0900

    ⚡ :: UserController

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,27 @@
 import { Controller } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Post, Get } from '@nestjs/common';
+import { Body, Param } from '@nestjs/common';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) { }
+
+    @Post()
+    create(@Body() createUserDto: CreateUserDto): Promise<any> {
+        return this.userService.create(createUserDto);
+    }
+
+    @Get()
+    findAll(): Promise<User[]> {
+        return this.userService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string): Promise<User> {
+        return this.userService.findOne(id);
+    }
+
+}
