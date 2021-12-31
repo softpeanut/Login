406ab05c348ba2e0ab8c9be4f452121d8e5eacf7
김범진 <sdpthf@naver.com>
2021-12-31 14:36:55 +0900

    📑 :: createDto

---
+++ src/user/dto/create.dto.ts
@@ -0,0 +1,17 @@
+import { IsString } from "class-validator";
+
+export class CreateUserDto {
+
+  @IsString()
+  userId: string;
+
+  @IsString()
+  userName: string;
+
+  @IsString()
+  password: string;
+
+  @IsString()
+  role: string;
+  
+}
