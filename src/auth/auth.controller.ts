f1801f0f99d13f93c474d433e0b56509e81a9c2d
김범진 <sdpthf@naver.com>
2022-01-01 21:53:52 +0900

    ⚡ :: authController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from 'src/guard/local-auth.guard';
+import { JwtAuthGuard } from 'src/guard/jwt-auth.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @UseGuards(LocalAuthGuard)
+    @Post('login')
+    async login(@Req() req) {
+        return this.authService.login(req.user);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('profile')
+    getProfile(@Req() req) {
+        return req.user;
+    }
+}
